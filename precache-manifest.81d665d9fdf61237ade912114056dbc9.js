2a5356d7a141279c26ae03e971e1d6c105bf1445
Ernais7 <tonybno@yahoo.fr>
2024-11-15 12:00:22 +0100

    Updates

---
+++ precache-manifest.81d665d9fdf61237ade912114056dbc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257db9ba3240333ee0a18cda53d9cf20",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "58ad55c9ce0b21683b75",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "bbe18361e446d6f74738",
+    "url": "/Portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "58ad55c9ce0b21683b75",
+    "url": "/Portfolio/static/js/2.e1b7ea4e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.e1b7ea4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbe18361e446d6f74738",
+    "url": "/Portfolio/static/js/main.39063ecb.chunk.js"
+  },
+  {
+    "revision": "c125469937b460e6de34",
+    "url": "/Portfolio/static/js/runtime-main.3379861c.js"
+  }
+]);
