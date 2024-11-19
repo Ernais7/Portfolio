30e5f056fb44ad062aa55732b4e4af079d568217
Ernais7 <tonybno@yahoo.fr>
2024-11-19 22:00:01 +0100

    feat: adding images and changing text

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -37,13 +37,13 @@ class Experience extends Component {
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<i className="fab fa-angular experience-icon"></i>}
+            icon={<i className={`${work.icon} experience-icon`}></i>}
             key={i}
           >
             <div style={{ textAlign: "left", marginBottom: "4px" }}>
               {mainTech}
             </div>
-
+        
             <h3
               className="vertical-timeline-element-title"
               style={{ textAlign: "left" }}
@@ -58,7 +58,7 @@ class Experience extends Component {
             </h4>
             <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
           </VerticalTimelineElement>
-        );
+        );        
       });
     }
 
